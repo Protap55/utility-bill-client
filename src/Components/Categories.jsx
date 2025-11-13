058105042440f78f6acb08730af3917ca9daa57d
protap55 <protapdutta500@gmail.com>
2025-11-13 19:37:24 +0600

    category done

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -37,19 +37,19 @@ const Categories = () => {
 
       <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-6 sm:gap-8 max-w-6xl mx-auto px-4 sm:px-6">
         {categories.map((cat, index) => (
-          // <div
-          //   key={index}
-          //   className={`rounded-3xl shadow-xl p-6 sm:p-8 flex flex-col items-center justify-center text-center 
-          //     bg-gradient-to-br ${cat.color} text-white transition-all duration-300 hover:scale-105 cursor-pointer`}
-          // >
-          //   <div className="mb-3 sm:mb-4">{cat.icon}</div>
-          //   <h3 className="text-xl sm:text-2xl font-semibold mb-2">
-          //     {cat.name}
-          //   </h3>
-          //   <p className="text-xs sm:text-sm opacity-90 font-medium">
-          //     {cat.desc}
-          //   </p>
-          // </div>
+          <div
+            key={index}
+            className={`rounded-3xl shadow-xl p-6 sm:p-8 flex flex-col items-center justify-center text-center 
+              bg-gradient-to-br ${cat.color} text-white transition-all duration-300 hover:scale-105 cursor-pointer`}
+          >
+            <div className="mb-3 sm:mb-4">{cat.icon}</div>
+            <h3 className="text-xl sm:text-2xl font-semibold mb-2">
+              {cat.name}
+            </h3>
+            <p className="text-xs sm:text-sm opacity-90 font-medium">
+              {cat.desc}
+            </p>
+          </div>
         ))}
       </div>
     </section>
